3e41ebed2bb4052bb3a1419649dddad32659fcac
joroosen <Fak3l0v3>
2020-12-08 15:57:12 +0100

    cleanup

--- 8.py
+++ 8.py
@@ -1,49 +1,49 @@
 import linecache
 
-changedLines = []
-accNumber = 0
-lineNumber = 1
+changed_lines = []
+acc_total = 0
+linenumber = 1
 alreadychecked = []
-line = linecache.getline("8_input.txt", lineNumber-1)
+line = linecache.getline("8_input.txt", linenumber - 1)
 
 
-def setInputdata(init):
+def set_inputdata(init):
     with open("8_input.txt", "r") as f:
         inputdata = f.readlines()
         if init != 0:
             for num, line1 in enumerate(inputdata, 1):
-                if num not in changedLines:
+                if num not in changed_lines:
                     if line1.startswith("jmp"):
                         inputdata[num-1] = "nop" + line1[3:]
-                        changedLines.append(num)
+                        changed_lines.append(num)
                         break
                     elif line1.startswith("nop"):
                         inputdata[num-1] = "jmp" + line1[3:]
-                        changedLines.append(num)
+                        changed_lines.append(num)
                         break
         return inputdata
 
 
 # fill input data for init
-inputData = setInputdata(0)
+inputdata = set_inputdata(0)
 
 
-while lineNumber <= len(inputData):
-    line = inputData[lineNumber-1]
-    if lineNumber in alreadychecked:
-        accNumber = 0
+while linenumber <= len(inputdata):
+    line = inputdata[linenumber - 1]
+    if linenumber in alreadychecked:
+        acc_total = 0
         alreadychecked.clear()
-        lineNumber = 1
-        inputData = setInputdata(1)
+        linenumber = 1
+        inputdata = set_inputdata(1)
     else:
-        alreadychecked.append(lineNumber)
+        alreadychecked.append(linenumber)
         if line.startswith("nop"):
-            lineNumber += 1
+            linenumber += 1
         elif line.startswith("acc"):
-            lineNumber += 1
-            accNumber = accNumber + int(line.partition(" ")[2])
+            linenumber += 1
+            acc_total = acc_total + int(line.partition(" ")[2])
         elif line.startswith("jmp"):
-            lineNumber = lineNumber + int(line.partition(" ")[2])
+            linenumber = linenumber + int(line.partition(" ")[2])
 
 
-print("Result for day 8, part 2: " + str(accNumber))
+print("Result for day 8, part 2: " + str(acc_total))
