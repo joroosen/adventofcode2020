2191cfad99f93c26234258813883e7b8ce057ce8
joroosen <Fak3l0v3>
2020-12-13 01:46:16 +0100

    Day 12 part 2 - changes (still wrong)

--- 12_part2.py
+++ 12_part2.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 pos = [0,0]
+pos_1 = ["N", "E"]
 east = 0
 north = 0
 dirs = ["N", "E", "S", "W"]
@@ -8,53 +9,118 @@ facing = 1
 nose = dirs[facing]
 waypoint_position = [1, 10]
 
-def navigate(value, waypoint_pos):
-    if dirs[0] == "N":
-        pos[0] = pos[0] + (waypoint_pos[0] * value)
-    elif dirs[0] == "E":
-        pos[1] = pos[1] + (waypoint_pos[1] * value)
-    elif dirs[0] == "S":
-        pos[0] = pos[0] - (waypoint_pos[0] * value)
-    elif dirs[0] == "W":
-        pos[1] = pos[1] - (waypoint_pos[1] * value)
 
-    if dirs[1] == "N":
-        pos[0] = pos[0] + (waypoint_pos[0] * value)
-    elif dirs[1] == "E":
-        pos[1] = pos[1] + (waypoint_pos[1] * value)
-    elif dirs[1] == "S":
-        pos[0] = pos[0] - (waypoint_pos[0] * value)
-    elif dirs[1] == "W":
-        pos[1] = pos[1] - (waypoint_pos[1] * value)
+def navigate(value, waypoint_pos):
+    if dirs[0] == "N" or dirs[1] == "N":
+        if pos_1[0] == "N":
+            pos[0] = abs(pos[0] + (waypoint_pos[0] * value))
+        else:
+            if pos[0] - (waypoint_pos[0] * value) < 0:
+                pos_1[0] = "S"
+            pos[0] = abs(pos[0] - (waypoint_pos[0] * value))
+    if dirs[0] == "E" or dirs[1] == "E":
+        if pos_1[1] == "E":
+            pos[1] = abs(pos[1] + (waypoint_pos[1] * value))
+        else:
+            if pos[1] - (waypoint_pos[1] * value) < 0:
+                pos_1[1] = "W"
+            pos[1] = abs(pos[1] - (waypoint_pos[1] * value))
+    if dirs[0] == "S" or dirs[1] == "S":
+        if pos_1[0] == "S":
+            pos[0] = abs(pos[0] + (waypoint_pos[0] * value))
+        else:
+            if pos[0] - (waypoint_pos[0] * value) < 0:
+                pos_1[0] = "N"
+            pos[0] = abs(pos[0] - (waypoint_pos[0] * value))
+    if dirs[0] == "W" or dirs[1] == "W":
+        if pos_1[1] == "W":
+            pos[1] = abs(pos[1] + (waypoint_pos[1] * value))
+        else:
+            if pos[1] - (waypoint_pos[1] * value) < 0:
+                pos_1[1] = "E"
+            pos[1] = abs(pos[1] - (waypoint_pos[1] * value))
 
 def set_waypoint(direction, value):
     if direction == "N":
-        waypoint_position[0] = waypoint_position[0] + value
-    elif direction == "E":
-        waypoint_position[1] = waypoint_position[1] - value
-    elif direction == "S":
-        waypoint_position[0] = waypoint_position[0] + value
-    elif direction == "W":
-        waypoint_position[1] = waypoint_position[1] - value
-
-# function to turn the ship
-def turn(direction, degrees, facing1):
-    degrees = int(degrees/90)
-    temp = [0, 1, 2, 3]
-    facing = facing1
-    if direction == "L":
-        for a in range(degrees):
-            if facing < 1 and facing >= 0:
-                facing = temp[-1]
-            else:
-                facing = temp[facing] - 1
-    elif direction == "R":
-            for a in range(degrees):
-                if facing > 2 and facing < 4:
-                    facing = temp[0]
+        if dirs[0] == "S" or dirs[1] == "S":
+            if waypoint_position[0] - value < 0:
+                if dirs[0] == "E" or dirs[1] == "E":
+                    degrees = 1
+                    items = deque(dirs)
+                    dirs.clear()
+                    items.rotate(+degrees)
+                    for a in items:
+                        dirs.append(a)
                 else:
-                    facing = temp[facing] + 1
-    return int(facing)
+                    degrees = 1
+                    items = deque(dirs)
+                    dirs.clear()
+                    items.rotate(-degrees)
+                    for a in items:
+                        dirs.append(a)
+            waypoint_position[0] = abs(waypoint_position[0] - value)
+        else:
+            waypoint_position[0] = abs(waypoint_position[0] + value)
+    elif direction == "E":
+        if dirs[0] == "W" or dirs[1] == "W":
+            if waypoint_position[1] - value < 0:
+                if dirs[0] == "S" or dirs[1] == "S":
+                    degrees = 1
+                    items = deque(dirs)
+                    dirs.clear()
+                    items.rotate(+degrees)
+                    for a in items:
+                        dirs.append(a)
+                else:
+                    degrees = 1
+                    items = deque(dirs)
+                    dirs.clear()
+                    items.rotate(-degrees)
+                    for a in items:
+                        dirs.append(a)
+            waypoint_position[1] = abs(waypoint_position[1] - value)
+        else:
+            waypoint_position[1] = abs(waypoint_position[1] + value)
+    elif direction == "S":
+        if dirs[0] == "N" or dirs[1] == "N":
+            if waypoint_position[0] - value < 0:
+                if dirs[0] == "W" or dirs[1] == "W":
+                    degrees = 1
+                    items = deque(dirs)
+                    dirs.clear()
+                    items.rotate(+degrees)
+                    for a in items:
+                        dirs.append(a)
+                else:
+                    degrees = 1
+                    items = deque(dirs)
+                    dirs.clear()
+                    items.rotate(-degrees)
+                    for a in items:
+                        dirs.append(a)
+            waypoint_position[0] = abs(waypoint_position[0] - value)
+        else:
+            waypoint_position[0] = abs(waypoint_position[0] + value)
+    elif direction == "W":
+        if dirs[0] == "E" or dirs[1] == "E":
+            if waypoint_position[1] - value < 0:
+                if dirs[0] == "S" or dirs[1] == "S":
+                    degrees = 1
+                    items = deque(dirs)
+                    dirs.clear()
+                    items.rotate(+degrees)
+                    for a in items:
+                        dirs.append(a)
+                else:
+                    degrees = 1
+                    items = deque(dirs)
+                    dirs.clear()
+                    items.rotate(-degrees)
+                    for a in items:
+                        dirs.append(a)
+            waypoint_position[1] = abs(waypoint_position[1] - value)
+        else:
+            waypoint_position[1] = abs(waypoint_position[1] + value)
 
 
 with open("12_input.txt") as f:
@@ -74,11 +140,6 @@ for move in mylist:
         items.rotate(-degrees)
         for a in items:
             dirs.append(a)
-        items = deque(waypoint_position)
-        waypoint_position.clear()
-        items.rotate(-degrees)
-        for a in items:
-            waypoint_position.append(a)
     elif action == "L":
         degrees = int(int(value) / 90)
         items = deque(dirs)
@@ -86,11 +147,6 @@ for move in mylist:
         items.rotate(+degrees)
         for a in items:
             dirs.append(a)
-        items = deque(waypoint_position)
-        waypoint_position.clear()
-        items.rotate(+degrees)
-        for a in items:
-            waypoint_position.append(a)
     else:
         set_waypoint(action, value)
 
